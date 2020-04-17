7b54a8061f5e8eb02906fa7fd5cde47f2c938a97
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-17 23:53:22 +0500

    First draft of region controller

---
+++ app/Http/Controllers/RegionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Region;
+use Illuminate\Http\Request;
+
+class RegionController extends Controller
+{
+    public function index(Request $request)
+    {
+        $parentRegion = $request->parent_id ?: 0;
+
+        $regions =  Region::where('parent_id', $parentRegion)
+            ->with('subregions')
+            ->get();
+
+        return $regions;
+    }
+}
