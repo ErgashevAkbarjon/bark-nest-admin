79999036e530712ca35d6bdc7861e3fab90da860
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-17 23:30:25 +0500

    Disable timestamps

--- app/RegionType.php
+++ app/RegionType.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class RegionType extends Model
 {
+    public $timestamps = false;
+
     public function regions()
     {
         return $this->hasMany(Region::class);
