97080f54a5fa02d0c28a6e30af9ba3db5c0f86c9
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-17 22:49:00 +0500

    Scaffold regions

---
+++ app/Region.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Region extends Model
+{
+    public function outages()
+    {
+        return $this->hasMany(Outage::class);
+    }
+
+    public function type()
+    {
+        return $this->belongsTo(RegionType::class, 'region_type_id');
+    }
+
+    public function subregions()
+    {
+        return $this->hasMany(Region::class, 'parent_id');
+    }
+}
---
+++ database/migrations/2020_04_17_172401_create_regions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->integer('parent_id');
+            $table->integer('region_type_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('regions');
+    }
+}
