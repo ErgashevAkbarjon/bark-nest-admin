5c669ffc123d9de5e01ad8f2fd67e5c28e5d3417
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-17 23:32:01 +0500

    Add regions seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(RegionTypesSeeder::class);
+        $this->call(RegionsTableSeeder::class);
     }
 }
