4a9ecdabdd2bf547ed915c6ff2def93a8155f01e
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-19 18:31:40 +0500

    Remove footer

--- resources/js/components/MainLayout.vue
+++ resources/js/components/MainLayout.vue
@@ -2,7 +2,7 @@
     <div>
         <v-navigation-drawer v-model="drawer" app>
             <v-list dense>
-                <v-list-item link>
+                <v-list-item link href="/regions">
                     <v-list-item-action>
                         <v-icon>mdi-home</v-icon>
                     </v-list-item-action>
@@ -35,9 +35,6 @@
                 </v-row>
             </v-container>
         </v-content>
-        <v-footer color="indigo" app>
-            <span class="white--text">&copy; 2020</span>
-        </v-footer>
     </div>
 </template>
 
