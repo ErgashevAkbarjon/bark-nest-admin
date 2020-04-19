e3e5527eb6990f730f0ae6f7b6443a8c2b03629e
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-19 20:53:41 +0500

    Refactor

---
+++ app/BarknestImport/regions.php
@@ -0,0 +1,383 @@
+<?php
+
+
+return [
+    [
+        'id' => 1,
+        'name' => 'РРП',
+        'region_type_id' => $regionType,
+        'parent_id' => 0
+    ],
+    [
+        'id' => 2,
+        'name' => 'Согдийская область',
+        'region_type_id' => $regionType,
+        'parent_id' => 0
+    ],
+    [
+        'id' => 3,
+        'name' => 'Хатлонская область',
+        'region_type_id' => $regionType,
+        'parent_id' => 0
+    ],
+    [
+        'id' => 4,
+        'name' => 'ГБАО',
+        'region_type_id' => $regionType,
+        'parent_id' => 0
+    ],
+    [
+        'id' => 9,
+        'name' => 'Айни',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 14,
+        'name' => 'Ашт',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 16,
+        'name' => 'Файзабад',
+        'region_type_id' => $districtType,
+        'parent_id' => 1
+    ],
+    [
+        'id' => 17,
+        'name' => 'Гисар',
+        'region_type_id' => $districtType,
+        'parent_id' => 1
+    ],
+    [
+        'id' => 18,
+        'name' => 'Чиргатол',
+        'region_type_id' => $districtType,
+        'parent_id' => 1
+    ],
+    [
+        'id' => 19,
+        'name' => 'Нурабад',
+        'region_type_id' => $districtType,
+        'parent_id' => 1
+    ],
+    [
+        'id' => 20,
+        'name' => 'Рашт',
+        'region_type_id' => $districtType,
+        'parent_id' => 1
+    ],
+    [
+        'id' => 21,
+        'name' => 'Рагун',
+        'region_type_id' => $districtType,
+        'parent_id' => 1
+    ],
+    [
+        'id' => 22,
+        'name' => 'Рудаки',
+        'region_type_id' => $districtType,
+        'parent_id' => 1
+    ],
+    [
+        'id' => 23,
+        'name' => 'Шахринав',
+        'region_type_id' => $districtType,
+        'parent_id' => 1
+    ],
+    [
+        'id' => 24,
+        'name' => 'Тавилдара',
+        'region_type_id' => $districtType,
+        'parent_id' => 1
+    ],
+    [
+        'id' => 25,
+        'name' => 'Точикобод',
+        'region_type_id' => $districtType,
+        'parent_id' => 1
+    ],
+    [
+        'id' => 26,
+        'name' => 'Турсунзаде',
+        'region_type_id' => $districtType,
+        'parent_id' => 1
+    ],
+    [
+        'id' => 27,
+        'name' => 'Вахдат',
+        'region_type_id' => $districtType,
+        'parent_id' => 1
+    ],
+    [
+        'id' => 28,
+        'name' => 'Варзоб',
+        'region_type_id' => $districtType,
+        'parent_id' => 1
+    ],
+    [
+        'id' => 29,
+        'name' => 'Бобочон Гафуров',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 30,
+        'name' => 'Деваштич',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 31,
+        'name' => 'Исфара',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 32,
+        'name' => 'Истаравшан',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 33,
+        'name' => 'Чабор Расулов',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 34,
+        'name' => 'Канибадам',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 35,
+        'name' => 'Кухистони Мастчох',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 36,
+        'name' => 'Мастчох',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 37,
+        'name' => 'Панчакент',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 38,
+        'name' => 'Шахристон',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 39,
+        'name' => 'Спитамен',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 40,
+        'name' => 'Зафарабад',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ],
+    [
+        'id' => 41,
+        'name' => 'Балчуван',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 42,
+        'name' => 'Бохтар',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 43,
+        'name' => 'Дангара',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 44,
+        'name' => 'Фархор',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 45,
+        'name' => 'Хамадони',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 46,
+        'name' => 'Чиликул',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 47,
+        'name' => 'Чоми',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 48,
+        'name' => 'Ховалинг',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 49,
+        'name' => 'Хуросон',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 50,
+        'name' => 'Куляб',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 51,
+        'name' => 'Муминабад',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 52,
+        'name' => 'Нурек',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 53,
+        'name' => 'Носири Хусрав',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 54,
+        'name' => 'Панч',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 55,
+        'name' => 'Кабадиян',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 56,
+        'name' => 'Кумсангир',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 57,
+        'name' => 'Руми',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 58,
+        'name' => 'Сарбанд',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 59,
+        'name' => 'Шахритуз',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 60,
+        'name' => 'Шурабад',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 61,
+        'name' => 'Темурмалик',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 62,
+        'name' => 'Вахш',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 63,
+        'name' => 'Восеъ',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 64,
+        'name' => 'Яван',
+        'region_type_id' => $districtType,
+        'parent_id' => 3
+    ],
+    [
+        'id' => 65,
+        'name' => 'Дарваз',
+        'region_type_id' => $districtType,
+        'parent_id' => 4
+    ],
+    [
+        'id' => 66,
+        'name' => 'Ишкашим',
+        'region_type_id' => $districtType,
+        'parent_id' => 4
+    ],
+    [
+        'id' => 67,
+        'name' => 'Мургаб',
+        'region_type_id' => $districtType,
+        'parent_id' => 4
+    ],
+    [
+        'id' => 68,
+        'name' => 'Рошткалъа',
+        'region_type_id' => $districtType,
+        'parent_id' => 4
+    ],
+    [
+        'id' => 69,
+        'name' => 'Рушон',
+        'region_type_id' => $districtType,
+        'parent_id' => 4
+    ],
+    [
+        'id' => 70,
+        'name' => 'Шугнон',
+        'region_type_id' => $districtType,
+        'parent_id' => 4
+    ],
+    [
+        'id' => 71,
+        'name' => 'Ванч',
+        'region_type_id' => $districtType,
+        'parent_id' => 4
+    ],
+    [
+        'id' => 76,
+        'name' => 'Бустон',
+        'region_type_id' => $districtType,
+        'parent_id' => 2
+    ]
+];
