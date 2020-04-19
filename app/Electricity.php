53ea1540cd65fea53ceb38e3a9a0e5c416576219
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-19 22:15:38 +0500

    Configure for mass assignment

--- app/Electricity.php
+++ app/Electricity.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Electricity extends Model
 {
+    protected $guarded = [];
+
     public function region()
     {
         return $this->belongsTo(Region::class);
