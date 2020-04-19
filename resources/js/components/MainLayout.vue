41d913e0da6d7a485af0a810f37e66e1e5a80122
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-19 17:48:58 +0500

    Add main layout

---
+++ resources/js/components/MainLayout.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <v-navigation-drawer v-model="drawer" app>
+            <v-list dense>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-home</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <v-list-item link>
+                    <v-list-item-action>
+                        <v-icon>mdi-contact-mail</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title>Contact</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+
+        <v-app-bar app color="indigo" dark>
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+            <v-toolbar-title>Application</v-toolbar-title>
+        </v-app-bar>
+
+        <v-content>
+            <v-container class="fill-height" fluid>
+                <v-row align="center" justify="center">
+                    <v-col class="text-center">
+                        <slot></slot>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
+        <v-footer color="indigo" app>
+            <span class="white--text">&copy; 2020</span>
+        </v-footer>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        source: String
+    },
+    data: () => ({
+        drawer: false
+    })
+};
+</script>
