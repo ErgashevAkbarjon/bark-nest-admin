179055cb196399af6e1de09234f64ae4abe8a942
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-19 18:33:42 +0500

    Changed home path

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
-    Route::get('/home', 'HomeController@index')->name('home');
+    Route::get('/', 'HomeController@index')->name('home');
 
     Route::get('/regions', 'RegionController@index');
 });
