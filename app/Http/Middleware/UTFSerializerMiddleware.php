99fc0290085ec7d331c3f49cb1e717e4cedf2d36
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-20 20:31:40 +0500

    Add utf-8 serializer for json response

---
+++ app/Http/Middleware/UTFSerializerMiddleware.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class UTFSerializerMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+
+        return response()->json(
+            $response->original,
+            $response->status(),
+            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+            JSON_UNESCAPED_UNICODE
+        );
+    }
+}
