59c047aa1e00e28b0c8bd59f66c72c0c81049b4e
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-22 01:58:40 +0500

    Fix styling

--- resources/js/components/MainLayout.vue
+++ resources/js/components/MainLayout.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
-
-        <v-app-bar app color="indigo" dark clipped-left>
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-            <v-toolbar-title>Электроснабжение</v-toolbar-title>
+        <v-app-bar app color="indigo" clipped-left dark>
+            <v-toolbar-title class="ml-5">Электроснабжение</v-toolbar-title>
         </v-app-bar>
 
-        <v-navigation-drawer v-model="drawer" app clipped>
+        <v-navigation-drawer
+            v-model="drawer"
+            app
+            clipped
+            mini-variant
+            expand-on-hover
+        >
             <v-list dense>
                 <v-list-item link>
                     <v-list-item-action>
@@ -48,10 +52,10 @@ export default {
     data() {
         return {
             drawer: true
-        }
+        };
     },
     methods: {
-        isCurrentLink(link){
+        isCurrentLink(link) {
             return location.pathname == link;
         }
     }
