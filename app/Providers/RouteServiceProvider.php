40e2c07f9add3e74add72aef929d51a5fb30fdbc
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-23 02:56:02 +0500

    Change home page

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/regions';
+    public const HOME = '/electricity';
 
     /**
      * Define your route model bindings, pattern filters, etc.
