681ee7d650c95b41ff95aa487ec2c936fe964455
Ergashev Akbar <ergashev.akb@gmail.com>
2020-04-22 22:46:52 +0500

    Add CORS middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,6 +39,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             'throttle:60,1',
+            \App\Http\Middleware\CorsMiddleware::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\UTFSerializerMiddleware::class
         ],
