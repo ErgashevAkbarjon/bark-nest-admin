84125eee13a77566c3274758042b1e8487f5b6fe
Ergashev Akbar <ergashev.akb@gmail.com>
2020-08-13 14:58:20 +0500

    Refactor

---
+++ resources/js/components/DistrictElectricityForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-card outlined>
+        <v-card-subtitle>
+            {{ district.name }}
+        </v-card-subtitle>
+        <v-card-text>
+            <time-input
+                label="Часы"
+                v-model="district.hours"
+                :rules="requiredRule"
+            />
+            <v-text-field
+                label="Часы подачи днем"
+                outlined
+                v-model="district.dayPeriod"
+            />
+            <v-text-field
+                label="Часы подачи ночью"
+                outlined
+                v-model="district.nightPeriod"
+            />
+        </v-card-text>
+    </v-card>
+</template>
+
+<script>
+import TimeInput from "./TimeInput";
+
+export default {
+    props: {
+        district: Object
+    },
+    components: { TimeInput },
+}
+</script>
--- resources/js/components/pages/CreateElectricity.vue
+++ resources/js/components/pages/CreateElectricity.vue
@@ -34,28 +34,9 @@
                                 v-for="(district, i) in region.subregions"
                                 :key="i"
                             >
-                                <v-card outlined>
-                                    <v-card-subtitle>
-                                        {{ district.name }}
-                                    </v-card-subtitle>
-                                    <v-card-text>
-                                        <time-input
-                                            label="Часы"
-                                            v-model="district.hours"
-                                            :rules="requiredRule"
-                                        />
-                                        <v-text-field
-                                            label="Часы подачи днем"
-                                            outlined
-                                            v-model="district.dayPeriod"
-                                        />
-                                        <v-text-field
-                                            label="Часы подачи ночью"
-                                            outlined
-                                            v-model="district.nightPeriod"
-                                        />
-                                    </v-card-text>
-                                </v-card>
+                                <district-electricity-form 
+                                    :district="district"
+                                />
                             </v-col>
                         </v-row>
                         <div
@@ -73,11 +54,11 @@
 </template>
 
 <script>
-import TimeInput from "../TimeInput";
+import DistrictElectricityForm from "../DistrictElectricityForm";
 
 export default {
     props: ["regions"],
-    components: { TimeInput },
+    components: { DistrictElectricityForm },
     data() {
         return {
             date: null,
