4bebe94a3b758403d728e7ca491086bb080937a4
Ergashev Akbar <ergashev.akb@gmail.com>
2020-08-17 23:40:24 +0500

    Bug fix

--- resources/js/components/pages/Regions.vue
+++ resources/js/components/pages/Regions.vue
@@ -198,7 +198,7 @@ export default {
 
             return {
                 method: "POST",
-                action: "/regions/",
+                action: "/regions",
                 actionName: "Добавить"
             };
         },
