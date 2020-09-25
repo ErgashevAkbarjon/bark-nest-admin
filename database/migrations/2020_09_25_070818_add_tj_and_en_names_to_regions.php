bedd13d850b62ce15161326b1b480fe28f371049
Ergashev Akbar <ergashev.akb@gmail.com>
2020-09-25 07:13:40 +0500

    Add Tj and Eng region names

---
+++ database/migrations/2020_09_25_070818_add_tj_and_en_names_to_regions.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTjAndEnNamesToRegions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('regions', function (Blueprint $table) {
+            $table->string('name_tj')->after('name');
+            $table->string('name_en')->after('name_tj');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('regions', function (Blueprint $table) {
+            $table->dropColumn('name_tj');
+            $table->dropColumn('name_en');
+        });
+    }
+}
